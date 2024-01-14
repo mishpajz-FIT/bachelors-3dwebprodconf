f08bed8aeb1db39bd0e5db66cafa1443d5bfb3c8
Michal Dobeš <dobesmic@fit.cvut.cz>
2024-01-14 20:22:20 +0100

    ci: change githooks to use husky

--- .githooks/git-hooks-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This script will config git hook path into specific folder in your project. This script will invoked by maven build.
-# @author : Mak Sophea
-# @version : 1.0#
-#
-echo "config git hooksPath to .githooks folder for commit-msg and pre-push"
-git config core.hooksPath .githooks
