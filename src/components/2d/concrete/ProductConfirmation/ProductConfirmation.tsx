ff8a528f28eb8d60526f131049a1bb008978cc4f
Michal Dobeš <dobesmic@fit.cvut.cz>
2024-01-19 20:04:21 +0100

    feat: add product confirmation tile

--- src/components/2d/concrete/ProductConfirmation/ProductConfirmation.tsx
+++ src/components/2d/concrete/ProductConfirmation/ProductConfirmation.tsx
@@ -1,14 +1,27 @@
+import { useSnapshot } from "valtio";
+
+import { ProductConfirmationTile } from "./ProductConfirmationTile.tsx";
+import { UserProductStore } from "../../../../stores/UserProductStore.ts";
 import { ContainerHeader } from "../../universal/ContainerHeader.tsx";
 
 export const ProductConfirmation = () => {
+  const userProductSnap = useSnapshot(UserProductStore);
+
   return (
     <div className="flex select-none flex-col items-center justify-center bg-white p-4 dark:bg-gray-900">
       <div className="w-full md:w-4/5 lg:w-2/3">
         <ContainerHeader title={"Confirm configuration"} onClose={undefined} />
       </div>
-      <div className="flex w-full flex-col justify-center gap-4 md:w-4/5 lg:w-2/3">
-        Test
-      </div>
+      <ul className="d:w-4/5 flex w-full flex-col justify-center rounded-xl bg-slate-50 lg:w-2/3 dark:bg-slate-800">
+        {Object.keys(userProductSnap.components).map((componentId, index) => (
+          <li
+            key={componentId}
+            className={`${index !== Object.keys(userProductSnap.components).length - 1 ? "border-b dark:border-b-gray-700" : ""}`}
+          >
+            <ProductConfirmationTile componentId={componentId} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/2d/concrete/ProductConfirmation/ProductConfirmationTile.tsx
+++ src/components/2d/concrete/ProductConfirmation/ProductConfirmationTile.tsx
@@ -32,16 +32,50 @@ export const ProductConfirmationTile = ({
   }
 
   return (
-    <div className="flex flex-row">
-      <SkeletonImage
-        src={componentSpec.imageUrl}
-        alt={componentSpec.name}
-        className="pointer-events-none inset-1 h-20 w-20 rounded"
-      />
+    <div className="flex flex-col p-4">
+      <div className="mb-2 flex flex-row items-center">
+        <SkeletonImage
+          src={componentSpec.imageUrl}
+          alt={componentSpec.name}
+          className="pointer-events-none inset-1 h-16 w-16 rounded"
+        />
 
-      <div className="mb-1 grow">
-        <h2 className="truncate text-lg font-semibold">{componentSpec.name}</h2>
+        <div className="ml-2 grow">
+          <h2 className="truncate text-lg font-semibold">
+            {componentSpec.name}
+          </h2>
+        </div>
       </div>
+
+      {Object.entries(component.materials).map(([materialId, colorSpecId]) => {
+        const material = componentSpec.materialSpecs[materialId];
+        if (!material) {
+          throw new Error(`Material ${materialId} not found`);
+        }
+
+        const colorSpec = material.colorVariationsSpecs[colorSpecId];
+        if (!colorSpec) {
+          throw new Error(`Color ${colorSpecId} not found`);
+        }
+
+        return (
+          <div className="flex flex-col p-1" key={materialId}>
+            <h2 className="text-sm font-normal">{material.name}</h2>
+            <div className="flex flex-row items-center p-1">
+              <div
+                className="h-10 w-10 rounded shadow-sm ring-1 ring-gray-300 dark:ring-gray-600"
+                style={{
+                  backgroundColor: colorSpec.value,
+                }}
+              />
+
+              <span className="ml-2 select-none truncate text-sm font-light leading-tight text-gray-600 dark:text-gray-400">
+                {colorSpec.name}
+              </span>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
