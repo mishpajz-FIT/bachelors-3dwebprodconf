e6c6000d74f1d0d5fd19cc63cb4a473f8b5cd256
Michal Dobeš <dobesmic@fit.cvut.cz>
2024-02-26 00:26:29 +0100

    feat: remove component from other parts in admin

--- apps/admin/src/components/2d/ProductComposer/EditComponentSpecification/EditComponentSpecification.tsx
+++ apps/admin/src/components/2d/ProductComposer/EditComponentSpecification/EditComponentSpecification.tsx
@@ -31,6 +31,27 @@ export const EditComponentSpecification = ({
   }
 
   const remove = () => {
+    Object.keys(ProductStore.baseSpecs).forEach((baseSpecId) => {
+      if (ProductStore.baseSpecs[baseSpecId].component === componentSpecId) {
+        delete ProductStore.baseSpecs[baseSpecId];
+      }
+    });
+
+    Object.keys(ProductStore.componentSpecs).forEach((iterComponentSpecId) => {
+      const iterComponentSpec =
+        ProductStore.componentSpecs[iterComponentSpecId];
+      Object.keys(iterComponentSpec.mountingPointsSpecs).forEach(
+        (iterMountingPointId) => {
+          const iterMountPoint =
+            iterComponentSpec.mountingPointsSpecs[iterMountingPointId];
+          iterMountPoint.mountableComponents =
+            iterMountPoint.mountableComponents.filter(
+              (component) => component !== componentSpecId
+            );
+        }
+      );
+    });
+
     delete ProductStore.componentSpecs[componentSpecId];
     onClose();
   };
