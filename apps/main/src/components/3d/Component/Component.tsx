0c9df25d27378982220793a4209e4fa6200338b3
Michal Dobeš <dobesmic@fit.cvut.cz>
2024-05-06 19:27:33 +0200

    fix: position selection buttons to the center of the component

--- apps/main/src/components/3d/Component/Component.tsx
+++ apps/main/src/components/3d/Component/Component.tsx
@@ -1,11 +1,9 @@
-import { Bvh, Html } from "@react-three/drei";
+import { Bvh } from "@react-three/drei";
 import { lazy } from "react";
 import { Euler } from "three";
-import { snapshot } from "valtio";
 
 import { MountingPointButton } from "./MountingPointButton.tsx";
 import { useComponent } from "../../../hooks/useComponent.ts";
-import { ConfiguratorValuesStore } from "../../../stores/ConfiguratorValuesStore.ts";
 
 const ComponentModel = lazy(() => import("./ComponentModel.tsx"));
 
@@ -14,8 +12,6 @@ interface ComponentProps {
 }
 
 export const Component = ({ componentId }: ComponentProps) => {
-  const configuratorValuesSnap = snapshot(ConfiguratorValuesStore);
-
   const { component, componentSpec } = useComponent(componentId);
 
   return (
@@ -39,24 +35,7 @@ export const Component = ({ componentId }: ComponentProps) => {
               rotation={new Euler(...mountingPoint.rotation)}
             >
               {mountedComponentId ? (
-                <>
-                  <Component componentId={mountedComponentId} />
-                  <Html zIndexRange={[50, 0]}>
-                    <div
-                      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-                      hidden={!configuratorValuesSnap.showMountingPoints}
-                    >
-                      <button
-                        className="size-3 cursor-pointer rounded-full border border-gray-300 bg-[var(--primary-light)] dark:border-zinc-700 dark:bg-[var(--primary-dark)]"
-                        onClick={(event) => {
-                          event.stopPropagation();
-                          ConfiguratorValuesStore.selectedComponentId =
-                            mountedComponentId;
-                        }}
-                      ></button>
-                    </div>
-                  </Html>
-                </>
+                <Component componentId={mountedComponentId} />
               ) : (
                 <MountingPointButton
                   componentId={componentId}
--- apps/main/src/components/3d/Component/ComponentModel.tsx
+++ apps/main/src/components/3d/Component/ComponentModel.tsx
@@ -2,10 +2,11 @@ import { Render } from "@3dwebprodconf/shared/src/components/3d/Render.tsx";
 import { useDarkMode } from "@3dwebprodconf/shared/src/hooks/useDarkMode.ts";
 import { Outlines, useGLTF } from "@react-three/drei";
 import { ThreeEvent } from "@react-three/fiber";
-import { useEffect, useMemo, useRef } from "react";
-import { Color, Euler, Group, Material } from "three";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Box3, Color, Euler, Group, Material, Matrix4, Vector3 } from "three";
 import { useSnapshot } from "valtio";
 
+import { SelectionButton } from "./SelectionButton.tsx";
 import { globalConfig } from "../../../configurations/Config.ts";
 import { useComponent } from "../../../hooks/useComponent.ts";
 import {
@@ -26,6 +27,10 @@ const ComponentModel = ({ componentId }: ComponentModelProps) => {
 
   const { scene, materials } = useGLTF(componentSpec.modelUrl);
   const groupRef = useRef<Group>(null);
+
+  const [centerPosition, setCenterPosition] = useState<Vector3 | undefined>(
+    undefined
+  );
 
   const customMaterials = useMemo(
     () =>
@@ -72,40 +77,62 @@ const ComponentModel = ({ componentId }: ComponentModelProps) => {
     ConfiguratorValuesStore.selectedComponentId = componentId;
   };
 
+  useEffect(() => {
+    if (!groupRef.current) return;
+    groupRef.current.updateMatrixWorld(true);
+    const boundingBox = new Box3().setFromObject(groupRef.current);
+    const center = new Vector3();
+    boundingBox.getCenter(center);
+    const inverseMatrix = new Matrix4()
+      .copy(groupRef.current.matrixWorld)
+      .invert();
+    setCenterPosition(center.applyMatrix4(inverseMatrix));
+  }, []);
+
   return (
-    <group ref={groupRef}>
-      <group
-        onClick={onSelect}
-        position={componentSpec.positionOffset}
-        rotation={
-          componentSpec.rotationOffset
-            ? new Euler(...componentSpec.rotationOffset)
-            : undefined
-        }
-        scale={componentSpec.scaleOffset}
-      >
-        <Render
-          object={scene}
-          materialOverrides={customMaterials}
-          userData={{
-            componentId: componentId,
-            ignoreCollisions: componentSpec.ignoreCollisions,
-          }}
-          dontMatrixAutoUpdate={true}
-        >
-          <Outlines
-            thickness={4}
-            screenspace={true}
-            color={
-              isDarkMode
-                ? globalConfig.config.spatialUi.selectionColors.outline.dark
-                : globalConfig.config.spatialUi.selectionColors.outline.light
-            }
-            visible={componentId === configuratorValuesSnap.selectedComponentId}
+    <>
+      <group ref={groupRef}>
+        {centerPosition && (
+          <SelectionButton
+            componentId={componentId}
+            position={centerPosition}
           />
-        </Render>
+        )}
+        <group
+          onClick={onSelect}
+          position={componentSpec.positionOffset}
+          rotation={
+            componentSpec.rotationOffset
+              ? new Euler(...componentSpec.rotationOffset)
+              : undefined
+          }
+          scale={componentSpec.scaleOffset}
+        >
+          <Render
+            object={scene}
+            materialOverrides={customMaterials}
+            userData={{
+              componentId: componentId,
+              ignoreCollisions: componentSpec.ignoreCollisions,
+            }}
+            dontMatrixAutoUpdate={true}
+          >
+            <Outlines
+              thickness={4}
+              screenspace={true}
+              color={
+                isDarkMode
+                  ? globalConfig.config.spatialUi.selectionColors.outline.dark
+                  : globalConfig.config.spatialUi.selectionColors.outline.light
+              }
+              visible={
+                componentId === configuratorValuesSnap.selectedComponentId
+              }
+            />
+          </Render>
+        </group>
       </group>
-    </group>
+    </>
   );
 };
 
